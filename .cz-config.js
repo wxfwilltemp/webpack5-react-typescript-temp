76b88c5eb7b6fbe2c8d9bf2aa1aaea3b4c84355e
尉新飞 <18810563686@163.com>
2022-03-30 17:50:20 +0800

    fix: 优化cz-config文件

--- .cz-config.js
+++ .cz-config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: will
  * @Date: 2022-03-28 17:20:11
- * @LastEditTime: 2022-03-30 17:14:23
+ * @LastEditTime: 2022-03-30 17:47:54
  * @LastEditors: will
  * @Description:
  */
@@ -27,7 +27,7 @@ module.exports = {
     subject: '请简要描述提交（必填）',
     body: '请输入详细描述（可选）',
     footer: '请选择要关闭的issue（可选）',
-    confirmCommit: '确认要使用以上信息提交？（y/n）',
+    confirmCommit: '确认提交？（y/n）',
   },
 
   // 跳过步骤
