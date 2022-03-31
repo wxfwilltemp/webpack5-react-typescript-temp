1a2d9be133230f332a215a9e133645162bd0c28c
尉新飞 <18810563686@163.com>
2022-03-31 09:37:43 +0800

    style: 优化格式

--- .cz-config.js
+++ .cz-config.js
@@ -1,14 +1,14 @@
 /*
  * @Author: will
  * @Date: 2022-03-28 17:20:11
- * @LastEditTime: 2022-03-30 17:47:54
+ * @LastEditTime: 2022-03-30 18:05:59
  * @LastEditors: will
  * @Description:
  */
 module.exports = {
   // 可选类型
   types: [
-    { value: 'feat', name: 'feat:      新功能6' },
+    { value: 'feat', name: 'feat:      新功能' },
     { value: 'fix', name: 'fix:       修复' },
     { value: 'docs', name: 'docs:      文档变更' },
     { value: 'style', name: 'style:     代码格式（不影响代码运行的变动）' },
